50faebfefb205515473568173ad3b9217a9609b0
Nils Martel <nils.martel@graphmasters.net>
2020-06-15 10:41:18 +0200

    Fix Generix syntax

--- generic.sol
+++ generic.sol
@@ -15,3 +15,14 @@ function map(list: List I, f: I -> R) -> List R =
     match list
         is [] then []
         or [elem, ..rest] then [f elem, .. map rest]
+
+
+-- can also be written as
+
+function map(list: List T, f: T -> R) I, R -> List R =
+    match list
+        is [] then []
+        or [elem, ..rest] then [f elem] ++ map rest
+
+
+function subtract(left: A, right: A) A where Neg(A, A) and Add(A, A, A) -> A = left + -right
