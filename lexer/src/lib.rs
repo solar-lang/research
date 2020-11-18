9704146cf17f8d6f4a008c42fb62086e90d51701
Nils Martel <nils.martel@graphmasters.net>
2020-11-18 13:03:05 +0100

    Set up keywords parsing

--- lexer/src/lib.rs
+++ lexer/src/lib.rs
@@ -1,7 +1,53 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use logos::Logos;
+
+#[derive(Logos, Debug, PartialEq)]
+enum Token {
+
+    // Keywords
+    #[token("let")]
+    LetKeyword,
+    #[token("set")]
+    SetKeyword,
+    #[token("get")]
+    GetKeywor,
+    #[token("for")]
+    ForKeyword,
+    #[token("while")]
+    WhileKeyword,
+    #[token("if")]
+    IfKeyword,
+    #[token("else")]
+    ElseKeyword,
+    #[token("then")]
+    ThenKeyword,
+    #[token("function")]
+    FunctionKeyword,
+    #[token("return")]
+    ReturnKeyword,
+    #[token("break")]
+    BreakKeyword,
+    #[token("match")]
+    MatchKeyword,
+    #[token("generic")]
+    GenericKeyword,
+    #[token("type")]
+    TypeKeyword,
+    #[token("is")]
+    IsKeyword,
+    #[token("or")]
+    OrKeyword,
+    #[token("in")]
+    InKeyword,
+    #[token("const")]
+    ConstKeyword,
+    #[token("use")]
+    AsyncKeyword,
+    #[token("async")]
+    UseKeyword,
+
+    // Logos requires one token variant to handle errors,
+    // it can be named anything you wish.
+    #[error]
+    // #[regex(r"[ \t\n\f]+", logos::skip)]
+    Error,
 }
