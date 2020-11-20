3c202f8d99459a03a7a5e9aecd160b102d56c9a5
Nils Martel <nils.martel@graphmasters.net>
2020-11-20 16:38:40 +0100

    util types for parser

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -1,7 +1,10 @@
+mod generics;
 mod identifier;
 mod keyword;
 mod parse;
+mod structs;
 mod util;
+
 pub(crate) use parse::Parse;
 
 pub(crate) use nom_locate::{position, LocatedSpan};
--- parser/src/parse.rs
+++ parser/src/parse.rs
@@ -1,4 +1,4 @@
-use crate::util::ws;
+use crate::util::whitespace;
 use crate::Span;
 
 pub trait Parse<'a>
@@ -7,7 +7,7 @@ where
 {
     fn parse(s: Span<'a>) -> nom::IResult<Span<'a>, Self>;
     fn parse_ws(s: Span<'a>) -> nom::IResult<Span<'a>, Self> {
-        let (_, rest) = ws(s)?;
+        let (_, rest) = whitespace(s)?;
         Self::parse(rest)
     }
 }
