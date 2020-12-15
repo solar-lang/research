24f73aae89fd9bb4e79ff0954dad4604f5d245db
Nils Martel <nils.martel@graphmasters.net>
2020-12-15 17:20:34 +0100

    Include new code sample

---
+++ test-code/structures.sol
@@ -0,0 +1,25 @@
+using date.(now, Date)
+using time.Time
+
+-- denoting private fields with - and public ones with +
+-- this will automatically derive (pub function name(Person) -> String | function birthday(Person) -> Date | ... | set_hair &Person HairColor)
+type Person
+- name String
+- birthday Date
+- gender Gender
++ hair HairColor mut
+
+type Gender
+| Female
+| Male
+| Other
+
+type HairColor
+| Blond
+| Brown
+| Black
+| Red
+
+pub function age(p Person) -> Time = p:birthday - now
+
+pub function sample_person() -> Person = Person name="Peter" birthday = (date.ymd 2003 03 17) gender Gender.Male hair HairColor.Red
