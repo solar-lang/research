c81e3455ca2a3a2b14e96e3597778b2e10229648
Nils Martel <nils.martel@graphmasters.net>
2020-12-29 11:10:36 +0100

    Include further modules

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -4,10 +4,14 @@ mod identifier;
 mod keyword;
 mod parse;
 mod structs;
+mod token;
 mod types;
+mod unused;
 mod util;
 use nom_locate::LocatedSpan;
 
 pub(crate) use parse::Parse;
+pub use token::Token;
+pub use unused::Unused;
 
 pub type Span<'a> = LocatedSpan<&'a str>;
