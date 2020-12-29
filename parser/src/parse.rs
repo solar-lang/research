bc33aba0227a94de95daa9fd90f5d1f6904baf39
Nils Martel <nils.martel@graphmasters.net>
2020-12-29 11:10:19 +0100

    Update Parse Trait

--- parser/src/parse.rs
+++ parser/src/parse.rs
@@ -1,13 +1,13 @@
-use crate::util::whitespace;
-use crate::Span;
+use crate::{Span, Token, Unused};
+use nom::combinator::map;
 
 pub trait Parse<'a>
 where
     Self: Sized,
 {
-    fn parse(s: Span<'a>) -> nom::IResult<Span<'a>, Self>;
-    fn parse_ws(s: Span<'a>) -> nom::IResult<Span<'a>, Self> {
-        let (rest, _whitespace_characters) = whitespace(s).unwrap();
-        Self::parse(rest)
+    fn parse(s: Span<'a>) -> nom::IResult<Span<'a>, (Span<'a>, Self)>;
+    fn parse_ws(s: Span<'a>) -> nom::IResult<Span<'a>, Token<'a, Self>> {
+        let (rest, preceding) = Unused::parse_many0(s).unwrap();
+        map(Self::parse, |(span, content)| Token {preceding, span, content})(s)
     }
 }
