47d2b9e81e70cd6e7b6629345d6e4eb7bc37c74e
Nils Martel <nils.martel@graphmasters.net>
2020-12-29 11:01:08 +0100

    Make Tokens more useful

--- parser/src/token.rs
+++ parser/src/token.rs
@@ -1,4 +1,5 @@
-use crate::{Span, unused::Unused };
+use std::ops::Deref;
+use crate::{unused::Unused, Span};
 
 /// Wrapper holding part of the AST, includes information aboput predceding comments and the location of the Syntax Node.
 /// This way the complete Source Code should be recreatable from the AST
@@ -6,4 +7,12 @@ pub struct Token<'a, T> {
     pub preceding: Vec<Unused<'a>>,
     pub span: Span<'a>,
     pub content: T,
+}
+
+impl<'a, T> Deref for Token<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.content
+    }
 }
