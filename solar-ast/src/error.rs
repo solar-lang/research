9d7369d477d3d1c5aae1a4cc5460c2af37a4e585
Nils Martel <nilsmartel@yahoo.de>
2021-05-10 13:55:51 +0200

    Improve error type

--- solar-ast/src/error.rs
+++ solar-ast/src/error.rs
@@ -1,7 +1,34 @@
 use solar_tokenizer::Token;
 
+/// Error originated while parsing tokens into ast tree
 pub struct Error<'a> {
-    cause: Token<'a>
+    /// Token causing the error to arise
+    pub cause: Token<'a>,
+    /// Tokens that would have circumvented this error
+    pub expected: Option<Vec<Token<'a>>>,
+    /// Wether parsing can recover after this branch of decisions failed.
+    /// false per default
+    recoverable: bool,
+}
+
+impl<'a> Error<'a> {
+    pub fn at_token(cause: Token<'a>) -> Self {
+        Error {
+            cause,
+            expected: None,
+            recoverable: false,
+        }
+    }
+
+    pub fn recoverable(self) -> Self {
+        Error { recoverable: true, ..self}
+    }
+
+    pub fn expected(self, tokens: Vec<Token<'a>>) -> Self {
+        // doesn't really makes sense, that expected field may already be filled
+        assert_eq!(self.expected, None);
+        Error { expected: Some(tokens), ..self}
+    }
 }
 
 /// Error type originating while parsing
