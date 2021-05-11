0ab956089a1b446bbedf9d77042cc74d9987257e
Nils Martel <nilsmartel@yahoo.de>
2021-05-11 11:15:01 +0200

    Entry point for AST parsing

--- solar-ast/src/ast/mod.rs
+++ solar-ast/src/ast/mod.rs
@@ -4,12 +4,19 @@ pub mod body;
 pub mod expr;
 use body::{Import, FunctionOrTypeOrTest};
 use solar_tokenizer::Token;
-// use solar_tokenizer::Token;
+use crate::error::{Res, Error};
 
 /// Tree representation of the syntax of a solar file
 pub struct Ast<'a> {
     pub tokens: &'a [Token<'a>],
     pub imports: Vec<Import<'a>>,
     pub functions_and_types_and_tests: Vec<FunctionOrTypeOrTest<'a>>,
+}
+
+impl<'a> Ast<'a> {
+    pub fn parse(tokens: &'a[Token<'a>]) -> Res<'a, Self> {
+
+    }
+
 }
 
