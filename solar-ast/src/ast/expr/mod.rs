b2bd688a603fea003e2b6f993b969451e751e53e
Nils Martel <nilsmartel@yahoo.de>
2021-05-21 19:01:35 +0200

    Parse Block Expressions

--- solar-ast/src/ast/expr/mod.rs
+++ solar-ast/src/ast/expr/mod.rs
@@ -2,7 +2,12 @@ pub mod full;
 pub use full::FullExpression;
 type BFE<'a> = Box<FullExpression<'a>>;
 
-use nom::{combinator::map, branch::alt, sequence::delimited };
+use nom::{
+    branch::alt,
+    combinator::{map, opt},
+    multi::{many0, many1},
+    sequence::{delimited, pair, preceded},
+};
 
 use crate::ast::expr::when::When;
 use crate::ast::identifier::{FullIdentifier, Identifier};
@@ -44,14 +49,16 @@ struct Abs<'a> {
 
 impl<'a> Parse<'a> for Abs<'a> {
     fn parse(input: &'a str) -> Res<'a, Self> {
-        let (rest, expr) = delimited(keywords::ParenOpen::parse, FullExpression::parse_ws, keywords::ParenClose::parse_ws)(input)?;
-        let span = unsafe{from_to(input, rest)};
+        let (rest, expr) = delimited(
+            keywords::Abs::parse,
+            FullExpression::parse_ws,
+            keywords::Abs::parse_ws,
+        )(input)?;
+        let span = unsafe { from_to(input, rest) };
 
-        Ok((rest, Abs{span, expr}))
+        Ok((rest, Abs { span, expr }))
     }
 }
-
-
 
 // may as well just be some parenthesis
 struct Tuple<'a> {
@@ -59,9 +66,35 @@ struct Tuple<'a> {
     pub values: Vec<FullExpression<'a>>,
 }
 
+impl<'a> Parse<'a> for Tuple<'a> {
+    fn parse(input: &'a str) -> Res<'a, Self> {
+        let (rest, values) = delimited(
+            keywords::ParenOpen::parse,
+            many0(FullExpression::parse_ws),
+            keywords::ParenClose::parse_ws,
+        )(input)?;
+        let span = unsafe { from_to(input, rest) };
+
+        Ok((rest, Tuple { span, values }))
+    }
+}
+
 pub struct Array<'a> {
     pub span: &'a str,
     pub values: Vec<FullExpression<'a>>,
+}
+
+impl<'a> Parse<'a> for Array<'a> {
+    fn parse(input: &'a str) -> Res<'a, Self> {
+        let (rest, values) = delimited(
+            keywords::BracketOpen::parse,
+            many0(FullExpression::parse_ws),
+            keywords::BracketClose::parse_ws,
+        )(input)?;
+        let span = unsafe { from_to(input, rest) };
+
+        Ok((rest, Array { span, values }))
+    }
 }
 
 pub struct FunctionCall<'a> {
@@ -162,8 +195,6 @@ pub enum ClosureArgs<'a> {
     Multiple(Identifier<'a>),
 }
 
-
-
 pub struct BlockExpression<'a> {
     pub span: &'a str,
     pub parts: Vec<BlockExpressionPart<'a>>,
@@ -171,27 +202,183 @@ pub struct BlockExpression<'a> {
 
 impl<'a> Parse<'a> for BlockExpression<'a> {
     fn parse(input: &'a str) -> Res<'a, Self> {
-        unimplemented!();
+        // {
+        let (rest, _) = keywords::CurlyOpen::parse(input)?;
+        let (rest, parts) = many0(BlockExpressionPart::parse_ws)(rest)?;
+        let (rest, _) = keywords::CurlyClose::parse_ws(rest)?;
+
+        let span = unsafe { from_to(input, rest) };
+
+        Ok((rest, BlockExpression { span, parts }))
     }
 }
 
 pub enum BlockExpressionPart<'a> {
-    Let {
-        identifier: Identifier<'a>,
-        expr: FullExpression<'a>,
-    },
-    Return(Option<FullExpression<'a>>),
-    Break,
-    Next,
-    Expression(FullExpression<'a>),
-    Lopp(BlockExpression<'a>),
-    If {
-        condition: FullExpression<'a>,
-        then: BlockExpression<'a>,
-    },
-    For {
-        variable: Identifier<'a>,
-        over: FullExpression<'a>,
-        body: BlockExpression<'a>,
-    },
+    Let(Let<'a>),
+    Return(Return<'a>),
+    Break(&'a str),
+    Next(&'a str),
+    Loop(Loop<'a>),
+    If(If<'a>),
+    For(For<'a>),
+    FullExpression(FullExpression<'a>),
+    // ;
+    Separator(&'a str),
+}
+
+impl<'a> Parse<'a> for BlockExpressionPart<'a> {
+    fn parse(input: &'a str) -> Res<'a, Self> {
+        let brk = map(keywords::Break::parse, |keywords::Break { span }| {
+            BlockExpressionPart::Break(span)
+        });
+        let next = map(keywords::Next::parse, |keywords::Next { span }| {
+            BlockExpressionPart::Next(span)
+        });
+        let sep = map(
+            keywords::SemiColon::parse,
+            |keywords::SemiColon { span }| BlockExpressionPart::Separator(span),
+        );
+
+        alt((
+            map(Let::parse, BlockExpressionPart::Let),
+            map(Return::parse, BlockExpressionPart::Return),
+            brk,
+            next,
+            map(Loop::parse, BlockExpressionPart::Loop),
+            map(If::parse, BlockExpressionPart::If),
+            map(For::parse, BlockExpressionPart::For),
+            map(FullExpression::parse, BlockExpressionPart::FullExpression),
+            sep,
+        ))(input)
+    }
+}
+
+pub struct If<'a> {
+    pub span: &'a str,
+    pub condition: FullExpression<'a>,
+    pub then: BlockExpression<'a>,
+}
+
+impl<'a> Parse<'a> for If<'a> {
+    fn parse(input: &'a str) -> Res<'a, Self> {
+        // if
+        let (rest, _) = keywords::If::parse(input)?;
+        // <expr>
+        let (rest, condition) = FullExpression::parse_ws(rest)?;
+        // do
+        let (rest, _) = keywords::Do::parse_ws(rest)?;
+        // {<expr> ...}
+        let (rest, then) = BlockExpression::parse_ws(input)?;
+
+        let span = unsafe { from_to(input, rest) };
+
+        Ok((
+            rest,
+            If {
+                span,
+                condition,
+                then,
+            },
+        ))
+    }
+}
+
+pub struct For<'a> {
+    pub span: &'a str,
+    pub variable: Identifier<'a>,
+    pub over: FullExpression<'a>,
+    pub body: BlockExpression<'a>,
+}
+
+impl<'a> Parse<'a> for For<'a> {
+    fn parse(input: &'a str) -> Res<'a, Self> {
+        // for
+        let (rest, _) = keywords::For::parse(input)?;
+        // x
+        let (rest, variable) = Identifier::parse_ws(rest)?;
+        // in
+        let (rest, _) = keywords::In::parse_ws(rest)?;
+        // list     e.g. <expr>
+        let (rest, over) = FullExpression::parse_ws(rest)?;
+        // do
+        let (rest, _) = keywords::Do::parse_ws(rest)?;
+        // {<expr> ...}
+        let (rest, body) = BlockExpression::parse_ws(input)?;
+
+        let span = unsafe { from_to(input, rest) };
+
+        Ok((
+            rest,
+            For {
+                span,
+                variable,
+                over,
+                body,
+            },
+        ))
+    }
+}
+
+pub struct Loop<'a> {
+    pub span: &'a str,
+    pub body: BlockExpression<'a>,
+}
+
+impl<'a> Parse<'a> for Loop<'a> {
+    fn parse(input: &'a str) -> Res<'a, Self> {
+        // loop
+        let (rest, _) = keywords::Loop::parse(input)?;
+        // {<expr> ...}
+        let (rest, body) = BlockExpression::parse_ws(input)?;
+
+        let span = unsafe { from_to(input, rest) };
+
+        Ok((rest, Loop { span, body }))
+    }
+}
+
+pub struct Let<'a> {
+    pub span: &'a str,
+    pub identifier: Identifier<'a>,
+    pub expr: FullExpression<'a>,
+}
+
+impl<'a> Parse<'a> for Let<'a> {
+    fn parse(input: &'a str) -> Res<'a, Self> {
+        let (rest, (identifier, expr)) = pair(
+            delimited(
+                keywords::Let::parse,
+                Identifier::parse_ws,
+                keywords::Assign::parse_ws,
+            ),
+            FullExpression::parse_ws,
+        )(input)?;
+
+        let span = unsafe { from_to(input, rest) };
+
+        Ok((
+            rest,
+            Let {
+                span,
+                identifier,
+                expr,
+            },
+        ))
+    }
+}
+
+pub struct Return<'a> {
+    pub span: &'a str,
+    pub value: Option<FullExpression<'a>>,
+}
+
+impl<'a> Parse<'a> for Return<'a> {
+    fn parse(input: &'a str) -> Res<'a, Self> {
+        let (rest, value) =
+            preceded(keywords::Return::parse, opt(FullExpression::parse_ws))(input)?;
+
+        let span = unsafe { from_to(input, rest) };
+
+        Ok((rest, Return { span, value }))
+    }
 }
