59c2df05edea447152dde6ab0ee8982760ceb241
Nils Martel <nilsmartel@yahoo.de>
2021-06-03 09:15:38 +0200

    Implement full ast parsing

--- solar-ast/src/ast/mod.rs
+++ solar-ast/src/ast/mod.rs
@@ -13,3 +13,21 @@ pub struct Ast<'a> {
     pub imports: Vec<Import<'a>>,
     pub functions_and_types_and_tests: Vec<FunctionOrTypeOrTest<'a>>,
 }
+
+impl<'a> crate::parse::Parse<'a> for Ast<'a> {
+    fn parse(input: &'a str) -> crate::parse::Res<'a, Self> {
+        use nom::multi::many0;
+
+        let (rest, imports) = many0(Import::parse_ws)(input)?;
+        let (rest, functions_and_types_and_tests) = many0(FunctionOrTypeOrTest::parse_ws)(rest)?;
+
+        let span = unsafe { crate::util::from_to(input, rest) };
+
+        Ok((rest, Ast {
+            span,
+            imports,
+            functions_and_types_and_tests,
+        }))
+    }
+}
+
